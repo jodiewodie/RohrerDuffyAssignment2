612e26d07515148b383c1e9667447bcaca11737f
Jodie Rohrer <jodie.rohrer@hotmail.com>
2018-01-31 16:59:23 -0600

    Add project files.

---
+++ RohrerDuffyAssignment2/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RohrerDuffyAssignment2.Models
+{
+    public class Admin : User
+    {
+        public string ClubTitle { get; set; }
+        public string AcademicStanding { get; set; }
+    }
+}
---
+++ RohrerDuffyAssignment2/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RohrerDuffyAssignment2.Models
+{
+    public class Client : User
+    {
+        public string BusinessName { get; set; }
+        public string Title { get; set; }
+        public string AltContact { get; set; }
+    }
+}
