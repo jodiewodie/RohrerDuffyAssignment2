ce8055c0ca2069295d5cde84c7459e952f5905c0
Amanda Duffy <arduffy26@yahoo.com>
2018-01-31 17:26:07 -0600

    Amanda-Test

--- RohrerDuffyAssignment2/Startup.cs
+++ RohrerDuffyAssignment2/Startup.cs
@@ -24,6 +24,8 @@ namespace RohrerDuffyAssignment2
             services.AddMvc();
         }
 
+        //comment
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
